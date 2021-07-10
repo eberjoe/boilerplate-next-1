b69475bc18226cf4a6baa99072153f97442b85bb
eberjoe <eberjoe@gmail.com>
2021-07-10 13:39:05 -0300

    Add main types

---
+++ src/types.ts
@@ -0,0 +1,49 @@
+export enum SupportChannel {
+  HARDWARE,
+  NETWORK,
+  SOFTWARE
+}
+
+export type Location = {
+  cityName: string;
+  countryName: string;
+  timezone: string;
+};
+
+export const Locations: Record<string, Location> = {
+  BERLIN: {
+    cityName: 'Berlim',
+    countryName: 'Alemanha',
+    timezone: 'Europe/Berlin'
+  },
+  BOMBAY: {
+    cityName: 'Bombaim',
+    countryName: 'Índia',
+    timezone: 'Asia/Kolkata'
+  },
+  BOSTON: {
+    cityName: 'Bostom',
+    countryName: 'EUA',
+    timezone: 'America/New_York'
+  },
+  CHICAGO: {
+    cityName: 'Chicago',
+    countryName: 'EUA',
+    timezone: 'America/Chicago'
+  },
+  SAO_PAULO: {
+    cityName: 'São Paulo',
+    countryName: 'Brasil',
+    timezone: 'America/Sao_Paulo'
+  },
+  RIO: {
+    cityName: 'Rio de Janeiro',
+    countryName: 'Brasil',
+    timezone: 'America/Sao_Paulo'
+  },
+  MANCHESTER: {
+    cityName: 'Manchester',
+    countryName: 'Reino Unido',
+    timezone: 'Europe/London'
+  }
+};
